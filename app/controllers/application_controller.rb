d2fcd84b638b28a24c573bb4bb422d8d8bd9e9b6
EC2 Default User <ec2-user@ip-172-31-35-221.ap-northeast-1.compute.internal>
2025-06-05 02:22:08 +0000

    [update]ログイン機能、booksコントローラーの機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_paramerts, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_paramerts
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
